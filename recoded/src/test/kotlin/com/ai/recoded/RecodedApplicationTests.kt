196564f96eed874ebd6c2d29b59f951e9a175dfd
ekrusznis <ekrusznis@gmail.com>
2022-06-11 12:58:13 -0400

    Initial commit

---
+++ recoded/src/test/kotlin/com/ai/recoded/RecodedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ai.recoded
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecodedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
